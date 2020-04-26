97795d0b722e84dc8461dd3e0bbd7d5f1662f6be
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-04-25 19:54:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,33 +8,14 @@ function createTable()
 	document.body.appendChild(table);
 	
 	var row=document.createElement("TR");
-	document.getElementById("table1").appendChild(row);
+	table.appendChild(row);
 	
 	var cell=document.createElement("TD");
-	row.appendChild(celltext);
+	row.appendChild(cell);
+	
 	var celltext=document.createTextNode("cell");
 	cell.appendChild(celltext);
-	/*
-	for (var i=0; i<4; i++)
-	{
-		var row = table.insertRow(0);
-		for (var j=0; j<4; j++)
-		{
-			
-			var cell=row.insertCell(j);
-			
-			cell.innerHTML="cell";
-		}
-	}
-	*/
-	// Create an empty <tr> element and add it to the 1st position of the table:
-	//var row = table.insertRow(0);
-	//var cell1 = row.insertCell(0);
-	//var cell2 = row.insertCell(1);
 
-	// Add some text to the new cells:
-	//cell1.innerHTML = "NEW CELL1";
-	//cell2.innerHTML = "NEW CELL2";
 }
 
 
