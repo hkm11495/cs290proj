cddaf5b95f98cfe4eed3f8f8bee8a24eb4b51612
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-04-25 19:14:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //HW4 script
+
+function tableValues()
+{
+	var table=document.getElementById("table1");
+	var row=table.insertRow(0);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+
+	// Add some text to the new cells:
+	cell1.innerHTML = "NEW CELL1";
+	cell2.innerHTML = "NEW CELL2";
+}
+
+
+tableValues();
