9e1003da33c60010d553b688fc1d5330e397f6dd
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-04-25 19:23:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 //HW4 script
 
-function tableValues()
+function createTable()
 {
-	var table = document.getElementById("myTable");
+	
+	var table = document.createElement("TABLE");
 
+	for (var i=0; i<4; i++)
+	{
+		var row = table.insertRow(0);
+		for (var j=0; j<4; j++)
+		{
+			
+			var cell=row.insertCell(j);
+			
+			cell.innerHTML=i + "," + j;
+		}
+	}
+	
 	// Create an empty <tr> element and add it to the 1st position of the table:
-	var row = table.insertRow(0);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
+	//var row = table.insertRow(0);
+	//var cell1 = row.insertCell(0);
+	//var cell2 = row.insertCell(1);
 
 	// Add some text to the new cells:
-	cell1.innerHTML = "NEW CELL1";
-	cell2.innerHTML = "NEW CELL2";
+	//cell1.innerHTML = "NEW CELL1";
+	//cell2.innerHTML = "NEW CELL2";
 }
 
 
-tableValues();
+createTable();
