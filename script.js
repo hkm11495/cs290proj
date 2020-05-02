804ebb22c15540b19916edfc033e664fe8d1ebce
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-05-02 17:10:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function moveUp()
 
 function moveDown()
 {
-	if(selectedRow.nextElementSibling ==="undefined" || !selectedRow.nextElementSibling)
+	if(selectedCell.parentElement.nextElementSibling.firstElementChild ==="undefined" || !selectedCell.parentElement.nextElementSibling.firstElementChild)
 	{
 		return;
 	}
