5dbede9a62bd15151d4538ace0cefd61fad955e6
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-05-13 13:59:21 -0500

    Add files via upload

---
+++ hw5script.js
@@ -0,0 +1,15 @@
+app.get('/get-loopback-improved',function(req,res)
+{
+	var qParams = [];
+  
+	//make an array of object of the format name: parameter.
+	for (var p in req.query)
+	{
+
+	qParams.push({'name':p,'value':req.query[p]})
+
+	}
+	var context = {};
+	context.dataList = qParams;
+	res.render('get-loopback-improved', context);
+});
